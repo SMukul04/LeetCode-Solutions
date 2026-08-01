126d7c754c0b4a0809b1486834f8312ae34052ac
Mukul Singh <smukul9876@gmail.com>
2026-08-01 23:51:22 +0530

    Time: 0 ms (100%), Space: 8.7 MB (44.3%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    double solve(double x, long long n) {
+        if (n == 0)
+            return 1.0;
+
+        if (n % 2 == 0)
+            return solve(x * x, n / 2);
+
+        return x * solve(x * x, n / 2);
+    }
+
+    double myPow(double x, int n) {
+        long long N = n;
+
+        if (N < 0) {
+            x = 1.0 / x;
+            N = -N;
+        }
+
+        return solve(x, N);
+    }
+};
