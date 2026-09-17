4ef435a4ba8ddf4baeafb6f42b8cc6f598c0338e
Mukul Singh <smukul9876@gmail.com>
2026-09-17 13:08:38 +0530

    Time: 36 ms (29.68%), Space: 106.8 MB (48.24%) - LeetHub

---
+++ 2126-destroying-asteroids/2126-destroying-asteroids.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool asteroidsDestroyed(long long mass, vector<int>& asteroids) {
+        sort(asteroids.begin(), asteroids.end());
+        int i;
+
+        for(i = 0; i < asteroids.size(); i++){
+            if(asteroids[i] <= mass){
+                mass += asteroids[i];
+            }
+            else{
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
